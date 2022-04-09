132b757496751b0e88f245c8ba107a1d83dad28a
AudieTee <atahwa@gmail.com>
2022-04-08 22:39:32 -0400

    task2.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,15 @@
  * Description: Print the lowercase alphabet
  * Return: 0
  */
+
 int main(void)
 {
 	int c;
-for (c = 0; c < 26; ++c)
+       
+	for (c = 0; c < 26; ++c)
 		putchar('a' + c);
-		putchar('\n');
-		return(0);
-		}
+	
+        putchar('\n');
+	
+	return(0);
+}
