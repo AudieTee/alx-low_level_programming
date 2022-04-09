eb649caa7a0f9852c1cce018f193c2ec485a0034
AudieTee <atahwa@gmail.com>
2022-04-08 22:35:27 -0400

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,13 +6,17 @@
  * Description: Print lower and upper alphabet on the same line
  * Return: 0
  */
+
 int main(void)
 {
 	int c;
-   for (c = 0; c < 26; ++c)
+
+        for (c = 0; c < 26; ++c)
 		putchar('a' + c);
 	for (c = 0; c < 26; ++c)
 		putchar('A' + c);
-		putchar('\n');
-		return (0);
+
+	putchar('\n');
+
+	return (0);
 }
