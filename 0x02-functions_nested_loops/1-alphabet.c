af61a521e3fcf3a474d99260a00a21bb4f3d48d6
AudieTee <atahwa@gmail.com>
2022-04-12 19:23:44 -0700

    task 0 to 7

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
+
+
+2-print_alphabet_x10.c
+
+Ans:
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+
+}
