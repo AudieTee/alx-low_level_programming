87622a0257c695e0c0da7e5a57e05d1e32c0dc9e
AudieTee <atahwa@gmail.com>
2022-04-12 19:30:47 -0700

    task 1 correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -18,34 +18,3 @@ void print_alphabet(void)
 	_putchar('\n');
 
 }
-
-
-2-print_alphabet_x10.c
-
-Ans:
-
-#include "main.h"
-
-/**
- * print_alphabet_x10 - prints alphabet 10 times
- * Return:void
- */
-
-void print_alphabet_x10(void)
-{
-	char c;
-	int i = 0;
-
-	while (i < 10)
-	{
-		c = 'a';
-		while (c <= 'z')
-		{
-			_putchar(c);
-			c++;
-		}
-		_putchar('\n');
-		i++;
-	}
-
-}
