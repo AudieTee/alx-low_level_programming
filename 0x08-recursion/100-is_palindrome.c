60c08d1e793ba6ecb17fb7d96a30eb9eafbef1c8
AudieTee <atahwa@gmail.com>
2022-04-28 13:27:52 -0400

    task7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -30,3 +30,19 @@ int comparator(char *s, int n1, int n2)
 	}
 	return (0);
 }
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
+}
+
