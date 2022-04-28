a915e680a0bc0a646dff4fb8562e1caef93d1f38
AudieTee <atahwa@gmail.com>
2022-04-28 13:58:51 -0400

    task0 correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,19 @@
-#include "main.h"
+#include "holberton.h"
 /**
- *_memset -   fills memory with a constant byte
- *@s: pointer block of memory to fill
- *@b: value to set
- *@n: bytes of the memory
- *Return: dest
+ * _memset - fills memory with a constant byte.
+ * @s: source string
+ * @b: the contant byte for filling
+ * @n: lenght of buffer
+ * Return: new string.
  */
-
 char *_memset(char *s, char b, unsigned int n)
-
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	/*Delacring FOR*/
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		*(s + i) = b; /*add 1 position s*/
-
-	} /*END FOR*/
-
+		*(s + i) = b;
+		i++;
+	}
 	return (s);
-}
+}[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[B[C[C[C[C[C[C[C[C[C[C[Cmain[C[C[C[C[C.h"[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[C
