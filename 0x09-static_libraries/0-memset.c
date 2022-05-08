eb69dd157960d37e4bba9e71ca4bbb2d7f646e8f
AudieTee <atahwa@gmail.com>
2022-05-08 00:28:59 -0400

    tasks

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: source string
+ * @b: the contant byte for filling
+ * @n: lenght of buffer
+ * Return: new string.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[B[C[C[C[C[C[C[C[C[C[C[Cmain[C[C[C[C[C.h"[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[C
