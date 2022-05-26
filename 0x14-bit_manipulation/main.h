7dd92eac50482b144d0e9823278a92642aea225a
AudieTee <atahwa@gmail.com>
2022-05-26 17:20:33 -0400

    main

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -10,4 +10,5 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endi[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[B[B[C[C[C[C[C[C[C[C[CMAIN_H[C[C[C[AMAIN_H[C[C[C[B[B[B[B[B[B[B[B[B[B[B[B[D[D[D[D[D[D[D[D[Df[B
+#endif
+
